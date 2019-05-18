262da5fdf7c28604dc3ba90aae05804eea507673
Andy Wendt <andy@awendt.com>
2019-05-18 14:31:31 -0600

    Add controller and route

--- spec/features/user_visits_homepage_spec.rb
+++ spec/features/user_visits_homepage_spec.rb
@@ -1,19 +1,3 @@
-describe 'MyBehavior' do
-  before do
-    # Do nothing
-  end
-
-  after do
-    # Do nothing
-  end
-
-  context 'when condition' do
-    it 'succeeds' do
-      pending 'Not implemented'
-    end
-  end
-end
-
 feature "User visits homepage" do
   scenario "successfully" do
     expect(page).to have_css 'h1', text: 'Todos'
