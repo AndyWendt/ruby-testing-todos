af3ee4adc0fdc71289c05a82a43c44542ee79ce0
Andy Wendt <andy@awendt.com>
2019-05-18 16:12:36 -0600

    add new routes and views

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,4 +2,8 @@ class TodosController < ApplicationController
   def index
 
   end
+
+  def new
+    @todo = Todo.new
+  end
 end
---
+++ spec/features/user_creates_todo_spec.rb
@@ -0,0 +1,11 @@
+feature "User creates todo" do
+  scenario "successfully" do
+    visit root_path
+
+    click_on "Add a new todo"
+    fill_in "Title", with: "Buy Milk"
+    click_on "Submit"
+
+    expect(page).to have_css '.todos li', text: 'Buy Milk'
+  end
+end
