00529b6729e1376529e06801e9210a2af13a5cc8
Andy Wendt <andy@awendt.com>
2019-05-19 11:26:46 -0600

    Fix issue with routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   def authenticate
     if !signed_in?
-      redirect_to new_session_path
+      redirect_to new_sessions_path
     end
   end
 
