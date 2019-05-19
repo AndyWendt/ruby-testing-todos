72059e3d73227fc6ee4ba7e17cb52750a74164f9
Andy Wendt <andy@awendt.com>
2019-05-18 18:34:15 -0600

    change over to before action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  def authenticate
+    if !signed_in?
+      redirect_to new_session_path
+    end
+  end
+
+  def signed_in?
+    false
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "todos#index"
   resources :todos, only: [:index, :new, :create]
+  resource :session, only: [:new]
 end
