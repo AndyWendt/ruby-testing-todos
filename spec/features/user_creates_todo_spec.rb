1028d954ef22b8b1439b7aa3c181a57257fb62e3
Andy Wendt <andy@awendt.com>
2019-05-18 18:05:38 -0600

    Back to green

--- spec/features/user_creates_todo_spec.rb
+++ spec/features/user_creates_todo_spec.rb
@@ -1,6 +1,6 @@
 feature "User creates todo" do
   scenario "successfully" do
-    visit root_path
+    sign_in
 
     click_on "Add a new todo"
     fill_in "Title", with: "Buy Milk"
---
+++ spec/support/features/sign_in.rb
@@ -0,0 +1,5 @@
+module Features
+  def sign_in
+    visit root_path
+  end
+end
